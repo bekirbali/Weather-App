e9acb02c19c6b64e367f52b99195585dd30782ee
bekirbali <bfbali43@gmail.com>
2023-01-26 20:20:56 +0300

    weather app

---
+++ main.js
@@ -0,0 +1,8 @@
+const key = "6d8d685969d439d8178c3b7a901ebcf4";
+const fullKey =
+  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=6d8d685969d439d8178c3b7a901ebcf4";
+const searchBtn = document.querySelector("button");
+
+const cities = document.querySelector(".cities :nth-child(2)");
+
+console.log(cities);
