2dd025c8f388c966e93197580dfc1d37d61fc5b7
bekirbali <bfbali43@gmail.com>
2023-01-28 11:25:02 +0300

    upgrades

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const urlStart = "https://api.openweathermap.org/data/2.5/weather?q=";
 const citySearch = document.querySelector(".search-bar input");
 const cityName = document.querySelector(".city");
 const date = document.querySelector(".date");
+const day = document.querySelector(".day");
 const degree = document.querySelector(".degree");
 const weatherName = document.querySelector(".weather-name");
 const iconText = document.querySelector(".icon");
 const body = document.querySelector("body");
+const humidityP = document.querySelector(".humidity");
+const weatherDetails = document.querySelector(".weather-description");
+const windP = document.querySelector(".wind");
 
 const searchBtn = document.querySelector("button");
 
@@ -18,10 +22,25 @@ const istanbul = document.querySelector(".cities :nth-child(2)");
 const london = document.querySelector(".cities :nth-child(3)");
 const toronto = document.querySelector(".cities :nth-child(4)");
 
+window.addEventListener("load", () => {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=istanbul&units=metric&appid=6d8d685969d439d8178c3b7a901ebcf4"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      weatherOnLoad(data);
+    });
+});
+
+//? WITH SEARCH BUTTON
+
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const url = `${urlStart + citySearch.value + key}`;
 
+  let newCityName = citySearch.value;
+  console.log(newCityName);
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
@@ -31,50 +50,116 @@ searchBtn.addEventListener("click", (e) => {
   citySearch.closest("form").reset();
 });
 
-const weather = (info) => {
-  changeCityName(info);
-  weatherStatus(info);
-  bgChange(info);
-};
-
-const changeCityName = (city) => {
-  const { sys } = city;
-  const { country } = sys;
-
-  //! ******* CITY NAME *******
-
-  cityName.innerHTML = `${city.name},${country}`;
-};
-
-const weatherStatus = (data) => {
-  const { main, weather } = data;
-  const { humidity, temp } = main;
-  console.log(humidity, Math.floor(temp));
-
-  //! ******* DEGREE *******
-
-  const { main: mainWeather, description, icon } = weather[0];
-  degree.innerHTML = `${Math.floor(temp)}°`;
-
-  //! ******* ICON *******
-
-  iconText.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="icon" />`;
-
-  //! ******* WEATHER NAME *******
-
-  weatherName.innerHTML = `${mainWeather}`;
-};
-
-const bgChange = (city) => {
-  const { name } = city;
-  body.style.backgroundImage = `url('https://source.unsplash.com/1080x720/?${name})`;
-};
+//? WITH CITY NAMES
 
 cities.addEventListener("click", (e) => {
   if (!e.target.classList.contains("cities")) {
     let listedName = e.target.closest("li").innerHTML;
-    citySearch.value = listedName;
-    weather(e);
+    const url = `${urlStart + listedName + key}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        weather(data);
+        citySearch.closest("form").reset();
+      });
     return;
   }
 });
+
+//? MAIN FUNCTION
+
+const weather = (data) => {
+  changeCityName(data);
+  weatherStatus(data);
+  bgChange(data);
+  timeStamp(data);
+};
+
+const weatherOnLoad = (data) => {
+  changeCityName(data);
+  weatherStatus(data);
+  timeStamp(data);
+};
+
+//! ******* CITY NAME *******
+
+const changeCityName = (data) => {
+  const { sys } = data;
+  console.log(sys);
+  const { country } = sys;
+  console.log(country);
+  cityName.innerHTML = `${data.name}, ${country}`;
+};
+
+//! ******* WEATHER STATUS *******
+
+const weatherStatus = (data) => {
+  const { main, weather, wind } = data;
+  const { humidity, temp } = main;
+  console.log(humidity, Math.round(temp));
+
+  humidityP.innerHTML = `${humidity}%`;
+
+  //* ******* DEGREE *******
+
+  const { main: mainWeather, description, icon } = weather[0];
+  degree.innerHTML = `${Math.round(temp)}°`;
+
+  let newDescription = description
+    .split(" ")
+    .map((x) => x[0].toUpperCase() + x.slice(1))
+    .join(" ");
+
+  weatherDetails.lastElementChild.innerHTML = `${newDescription}`;
+
+  const { speed } = wind;
+  windP.innerHTML = `${speed}km/h`;
+
+  //* ******* ICON *******
+
+  iconText.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="icon" />`;
+
+  //* ******* WEATHER NAME *******
+
+  weatherName.innerHTML = `${mainWeather}`;
+};
+
+//! BACKGROUND CHANGE
+
+const bgChange = (city) => {
+  const { name } = city;
+  body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name})`;
+};
+
+//! TIME STAMP
+
+const timeStamp = (data) => {
+  let cityTime = data.dt;
+  cityTime = new Date(cityTime * 1000);
+  console.log(cityTime);
+  let today = new Date().getDay();
+  switch (today) {
+    case 0:
+      day.innerHTML = "Sunday";
+      break;
+    case 1:
+      day.innerHTML = "Monday";
+      break;
+    case 2:
+      day.innerHTML = "Tuesday";
+      break;
+    case 3:
+      day.innerHTML = "Wednesday";
+      break;
+    case 4:
+      day.innerHTML = "Thursday";
+      break;
+    case 5:
+      day.innerHTML = "Friday";
+      break;
+    case 6:
+      day.innerHTML = "Saturday";
+      break;
+  }
+};
